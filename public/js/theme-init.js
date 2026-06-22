17978c3d5955b878cf032bf6f9f7cd34008f9713
Delimo <delimo356@gmail.com>
2026-06-22 12:58:35 +0800

    new

--- public/js/theme-init.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  var e = localStorage.getItem("theme");
-  if (e === "dark" || e === "light") {
-    document.documentElement.setAttribute("data-theme", e);
-  } else if (matchMedia("(prefers-color-scheme:dark)").matches) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  }
-})();
